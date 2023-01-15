896c26dbb488089c74b53f3528da22e7765ac8b1
Aleksandr Melanchenko <alexnd@gmail.com>
2023-01-15 23:14:07 +0200

    fix postinstall

--- postinstall.js
+++ postinstall.js
@@ -2,9 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 const adaterFilename = 'adapter.js';
-const sourceFilePath = path.resolve(__dirname, 'node_modules', 'webrtc-adapter', 'out'  , adaterFilename);
-const targetFilePath = path.resolve(__dirname, adaterFilename);
+const sourceFilePath = path.resolve(__dirname, 'node_modules', 'webrtc-adapter', 'out', adaterFilename);
+const targetFilePath = path.resolve('.', adaterFilename);
+
+console.log('*from', sourceFilePath);
+console.log('*to', targetFilePath);
 
 if (!fs.existsSync(targetFilePath) && fs.existsSync(sourceFilePath)) {
   fs.copyFileSync(sourceFilePath, targetFilePath);
+  console.log('*copied');
 }
